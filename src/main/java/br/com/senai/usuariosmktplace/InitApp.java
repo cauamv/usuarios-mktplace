a945103dc8581d8d4d2920158ba0285cd9e5ee7a
cauamv <cauamvieira29@gmail.com>
2023-08-18 22:10:19 -0300

    Implementado pré-requisitos para desenvolvimento da regra de negócio

--- src/main/java/br/com/senai/usuariosmktplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/InitApp.java
@@ -1,13 +1,15 @@
 package br.com.senai.usuariosmktplace;
 
-import br.com.senai.usuariosmktplace.core.dao.DaoUsuario;
-import br.com.senai.usuariosmktplace.core.dao.FactoryDao;
-import br.com.senai.usuariosmktplace.core.domain.Usuario;
+import br.com.senai.usuariosmktplace.core.service.UsuarioService;
 
 public class InitApp {
 
 	public static void main(String[] args) {
-		
+		UsuarioService service = new UsuarioService();
+		System.out.println(service.removerAcentoDo("Pedro de Alcântara"));
+		System.out.println(service.fracionar("Pedro de Alcântara Francisco da Antônio dos João Carlos Xavier de Paula das Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon"));
+		System.out.println(service.gerarLoginPor("Pedro de Alcântara Francisco da Antônio dos João Carlos Xavier de Paula das Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon"));
+		System.out.println(service.gerarHashDa("jose123456"));
 	}
 
 }
