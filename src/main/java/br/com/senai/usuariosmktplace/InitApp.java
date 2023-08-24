8c6fbed6b3b23a5aafad51b71352d0bd0362ca0d
cauamv <cauamvieira29@gmail.com>
2023-08-23 22:07:47 -0300

    Finalizada a funcionalidade de crianção de novo usuario

--- src/main/java/br/com/senai/usuariosmktplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/InitApp.java
@@ -6,10 +6,7 @@ public class InitApp {
 
 	public static void main(String[] args) {
 		UsuarioService service = new UsuarioService();
-		System.out.println(service.removerAcentoDo("Pedro de Alcântara"));
-		System.out.println(service.fracionar("Pedro de Alcântara Francisco da Antônio dos João Carlos Xavier de Paula das Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon"));
-		System.out.println(service.gerarLoginPor("Pedro de Alcântara Francisco da Antônio dos João Carlos Xavier de Paula das Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon"));
-		System.out.println(service.gerarHashDa("jose123456"));
+		service.validar("jose 1234");
 	}
 
 }
